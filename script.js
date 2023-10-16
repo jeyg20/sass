e7807c56772d814b755a83a582601b5ddd4ccf44
Jeison <jei_son1516@hotmail.com>
2023-10-16 08:02:36 -0400

    Add responsive design breakpoints
    - Implemented responsive CSS styles for 768px, 1024px, and 1440px breakpoints
    - Adjusted layout and content to ensure proper display on various screen sizes
    - Tested and verified responsiveness on different devices and browsers

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const linksElement = document.getElementById('navLinks')
 const hamburgerElement = document.getElementById('navHamburger')
 
 //console.log('click')
-linksElement.style.display = 'none'
+//linksElement.style.display = 'none'
 
 hamburgerElement.addEventListener('touchstart', () => {
-    console.log(toString(linksElement.style.display))
-    if (linksElement.style.display == 'none') {
-        linksElement.style.display = 'flex'
+    if (linksElement.className === "icons") {
+        linksElement.className += ' responsive'
     } else {
-        console.log('false')
-        linksElement.style.display = 'none'
+        linksElement.className = "icons"
     }
 })
