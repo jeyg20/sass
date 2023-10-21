3bafa4817144223532fede2f9238315de2d0f5b7
Jeison <jei_son1516@hotmail.com>
2023-10-21 01:39:58 -0400

    Simplification of hamburger menu display toggle
    -replace the ternary if that concatenates and removes the
    class name for the toggle function.

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const linksElement = document.getElementById('navLinks')
 const hamburgerElement = document.getElementById('navHamburger')
 
-//console.log('click')
-//linksElement.style.display = 'none'
-
 hamburgerElement.addEventListener('touchstart', () => {
-    if (linksElement.className === "icons") {
-        linksElement.className += ' responsive'
-    } else {
-        linksElement.className = "icons"
-    }
+    linksElement.classList.toggle('responsive')
 })
