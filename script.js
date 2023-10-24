0a423f6ee29329d85d34e2d4555177818b4af106
Jeison <jei_son1516@hotmail.com>
2023-10-23 20:10:18 -0400

    hamburger touchstart event passive set true

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const linksElement = document.getElementById('navLinks')
 const hamburgerElement = document.getElementById('navHamburger')
 
 hamburgerElement.addEventListener('touchstart', () => {
-    linksElement.classList.toggle('responsive')
-})
+    linksElement.classList.toggle('responsive');
+},{ passive: true });
